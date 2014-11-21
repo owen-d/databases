5cd34019efe4e707e691a9618cbf25a69b9e907d
owen-d <ow.diehl@gmail.com>
2014-11-21 15:22:42 -0800

    sql schema set up

--- server/schema.sql
+++ server/schema.sql
@@ -1,9 +1,15 @@
+DROP DATABASE IF EXISTS chat;
+
 CREATE DATABASE chat;
 
 USE chat;
 
-CREATE TABLE messages (
+CREATE TABLE messages (msgID int(5) NOT NULL auto_increment, userID int(5), timeID int(5), room VARCHAR(20), message TEXT, PRIMARY KEY(msgID)
   /* Describe your table here.*/
+);
+CREATE TABLE time (timeID int(5) NOT NULL auto_increment, time TIMESTAMP(6), msgID int(5), PRIMARY KEY(timeID)
+);
+CREATE TABLE user (userID int(5), name VARCHAR(20)
 );
 
 /* Create other tables and define schemas for them here! */
